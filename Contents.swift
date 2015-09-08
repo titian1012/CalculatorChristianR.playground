adfdc6b67518559ea41fd9436cd699772c7271a8
titian1012 <titian1@yahoo.com>
2015-09-08 17:09:41 -0500

    CalculatorChristianR
    calc

---
+++ Contents.swift
@@ -0,0 +1,18 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var a:Float = 14
+var b:Float = 3
+
+var multiplication = a*b
+var division = a/b
+var addition = a+b
+var subtraction = a-b
+var percentage = b/a*100
+
+println("You multiplied \(a) and \(b) and got \(multiplication)")
+println("You divided \(a) and \(b) and got \(division)")
+println("You added \(a) and \(b) and got \(addition)")
+println("You subtracted \(a) and \(b) and got \(subtraction)")
+println("You wanted the percentage of \(a) and \(b) and got \(percentage) percent")
